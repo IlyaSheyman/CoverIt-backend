c89c9e03c4c2287ae9ee13f08c79dba3ac1fe4e7
Ilya Sheyman <ilyasheiman@yandex.ru>
2024-04-08 13:43:34 +0300

    refactor: added generations limit for release generator (post-test)

--- main-service/src/main/java/main_service/release/dto/ReleaseNewDto.java
+++ main-service/src/main/java/main_service/release/dto/ReleaseNewDto.java
@@ -2,6 +2,7 @@ package main_service.release.dto;
 
 import lombok.*;
 import main_service.playlist_card.cover.dto.CoverSmallDto;
+import main_service.user.dto.UserReleaseDto;
 import main_service.user.dto.UserSmallDto;
 
 import java.time.LocalDateTime;
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 public class ReleaseNewDto {
     private int id;
     private String title;
-    private UserSmallDto author;
+    private UserReleaseDto author;
     private CoverSmallDto cover;
     private LocalDateTime createdAt;
 }
